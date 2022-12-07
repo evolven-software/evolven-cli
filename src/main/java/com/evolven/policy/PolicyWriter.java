fe584f27c92dab4ac367cdeb34289a6166eb3132
Taras Gudz <taras.gudz@evolven.com>
2022-12-07 10:28:48 +0200

    add filter non-editable fields to pull policy

--- src/main/java/com/evolven/policy/PolicyWriter.java
+++ src/main/java/com/evolven/policy/PolicyWriter.java
@@ -1,11 +1,16 @@
 package com.evolven.policy;
 
+import com.evolven.common.YAMLUtils;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import javafx.util.Pair;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 public class PolicyWriter {
 
@@ -21,7 +26,14 @@ public class PolicyWriter {
     }
 
     public void write(File file, Policy policy) throws IOException {
-        mapper.writeValue(file, policy.getPolicy());
+        ObjectNode node = YAMLUtils.createObjectNode();
+        JsonNode policyNode = policy.getPolicy();
+        config.getEditablePolicyFields()
+                .stream()
+                .map(f -> new Pair<>(f, policyNode.get(f)))
+                .filter(p -> p.getValue() != null)
+                .forEach(p -> node.put(p.getKey(), p.getValue().asText()));
+        mapper.writeValue(file, node);
     }
 
 }
