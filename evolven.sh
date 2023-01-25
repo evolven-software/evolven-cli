bcffdafdad0589bb5e5d35b62cee24d6f3a7a0e9
Taras Gudz <taras.gudz@evolven.com>
2023-01-25 07:28:47 +0200

    add qoutes on paths in evolven.sh

--- evolven.sh
+++ evolven.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-ROOT_DIR=$(dirname ${SCRIPT_DIR})
+ROOT_DIR=$(dirname "${SCRIPT_DIR}")
 JAR_FILE_PATTERN='evolven-cli*.jar'
 
-JAR_FILE=$(ls ${ROOT_DIR}/lib/${JAR_FILE_PATTERN} 2> /dev/null)
-if [[ ! -f ${JAR_FILE} ]]; then
-  JAR_FILE=$(ls ${SCRIPT_DIR}/build/output/lib/${JAR_FILE_PATTERN} 2> /dev/null)
+JAR_FILE=$(ls "${ROOT_DIR}/lib/${JAR_FILE_PATTERN}" 2> /dev/null)
+if [[ ! -f "${JAR_FILE}" ]]; then
+  JAR_FILE=$(ls "${SCRIPT_DIR}/build/output/lib/${JAR_FILE_PATTERN}" 2> /dev/null)
 fi
-if [[ ! -f ${JAR_FILE} ]]; then
+if [[ ! -f "${JAR_FILE}" ]]; then
   echo Could not find Evolven CLI launcher.
   exit 1
 fi
 
-java -jar ${JAR_FILE} "$@"
+java -jar "${JAR_FILE}" "$@"
