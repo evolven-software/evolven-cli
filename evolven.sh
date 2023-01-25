11dc7a92e7ff794e3bf2081054501c59c4dd5acd
Taras Gudz <taras.gudz@evolven.com>
2023-01-25 16:52:34 +0200

    fix evolven.sh quotes

--- evolven.sh
+++ evolven.sh
@@ -3,10 +3,9 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 ROOT_DIR=$(dirname "${SCRIPT_DIR}")
 JAR_FILE_PATTERN='evolven-cli*.jar'
-
-JAR_FILE=$(ls "${ROOT_DIR}/lib/${JAR_FILE_PATTERN}" 2> /dev/null)
+JAR_FILE=$(ls "${ROOT_DIR}"/lib/${JAR_FILE_PATTERN} 2> /dev/null)
 if [[ ! -f "${JAR_FILE}" ]]; then
-  JAR_FILE=$(ls "${SCRIPT_DIR}/build/output/lib/${JAR_FILE_PATTERN}" 2> /dev/null)
+  JAR_FILE=$(ls "${SCRIPT_DIR}"/build/output/lib/${JAR_FILE_PATTERN} 2> /dev/null)
 fi
 if [[ ! -f "${JAR_FILE}" ]]; then
   echo Could not find Evolven CLI launcher.
